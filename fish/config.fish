f5182301199290132bb8f6dc0df8d87f1b79e27a
carpinterzeta <albcarpintero@gmail.com>
2025-05-09 12:23:36 +0200

    Create config.fish
    alias in fish with bat & lsd

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    alias ls='lsd'
+    alias l='lsd --almost-all --long'
+    alias llm='lsd --timesort --long'
+    alias lS='lsd --oneline --classic'
+    alias lt='lsd --tree --depth=2'
+    alias cat='bat'
+    # Commands to run in interactive sessions can go here
+end
